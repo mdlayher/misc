fa0a778dddc6d73a4c760c4a041ed4077f41cdd9
Matt Layher <mdlayher@gmail.com>
2014-07-02 16:35:29 -0400

    rust: tutorial through chapter 4

--- rust/src/hello-world.rs
+++ rust/src/hello-world.rs
@@ -1,3 +1,17 @@
+// main defines the program entry point
 fn main() {
-	println!("hello, world!");
+	// println!() is a macro which functions similar to C printf()
+	println!("hello, {}!\n", "world");
+
+	let item = "apple";
+	let price: f32 =
+		if item == "apple" {
+			1.00
+		} else if item == "banana" {
+			1.50
+		} else {
+			2.00
+		};
+
+	println!("The price of {} is {}", item, price);
 }
