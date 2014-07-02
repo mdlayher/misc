f38e980d1ca6968f74605058e9ee94a91cd3bd8e
Matt Layher <mdlayher@gmail.com>
2014-07-02 16:45:40 -0400

    rust: function calls and basic match statements

--- rust/src/hello-world.rs
+++ rust/src/hello-world.rs
@@ -3,7 +3,10 @@ fn main() {
 	// println!() is a macro which functions similar to C printf()
 	println!("hello, {}!\n", "world");
 
+	// All variables are immutable unless declared with "let mut"
 	let item = "apple";
+
+	// Declaring price using conditional statements
 	let price: f32 =
 		if item == "apple" {
 			1.00
@@ -14,4 +17,27 @@ fn main() {
 		};
 
 	println!("The price of {} is {}", item, price);
+
+	// Calling and using function return
+	println!("signum({}) = {}\n", -10i, signum(-10));
+
+	// The "match" statement is a more generalized version of C "switch", which accepts expressions
+	let num = 1i;
+	match num {
+		// Single condition
+		0     => { println!("zero") }
+		// One or the other
+		1 | 2 => { println!("one or two") }
+		// Range of conditions
+		3..10 => { println!("three to ten") }
+		// match is exhaustive, so a wildcard case is required at compile-time
+		_     => { println!("something else") }
+	}
+}
+
+// signum returns the sign of an integer
+fn signum(x: int) -> int {
+	if x < 0 { -1 }
+	else if x > 0 { 1 }
+	else { 0 }
 }
